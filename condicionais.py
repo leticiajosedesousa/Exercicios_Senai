3f8df784b957bc596137f5f405218c1497028550
leticiajosedesousa <leticiajose42@gmail.com>
2025-05-13 09:47:52 -0300

    Add files via upload

---
+++ condicionais.py
@@ -0,0 +1,18 @@
+# as condiconais testam valores logicos "verdadeiro" ou "falso" (true) (false) tipo: (bool)
+a = 10
+b = 10
+c = 10
+
+#print(a)
+#print(b)
+#print(a == b)
+#print(a == c)
+
+if a == c:
+    print('verdadeiro')
+    print('aeee')
+    print('uhhuuuu')
+else:
+    print('que pena')
+
+print('FIM')
